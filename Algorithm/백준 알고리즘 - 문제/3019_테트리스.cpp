646877c69757eff89a6d816f0e00932887f64d94
Jongwon Choi <cjw--@nate.com>
2019-10-04 20:52:27 +0900

    [mod] 백준 3019-테트리스 코드 간결화
    테트리스 블록의 모든 모양을 일일이 조건문으로 검사하는 방식에서, 테트리스 밑 바닥과 판 과의 높이차가 동일한지를 확인하는 방법으로 변경했다. 하나의 함수로 바닥면의 모양만 인자로 변경해서 함께 처리할 수 있어서 기존의 코드보다 5분의 1로 코드양이 줄었다.

--- Algorithm/백준 알고리즘 - 문제/3019_테트리스.cpp
+++ Algorithm/백준 알고리즘 - 문제/3019_테트리스.cpp
@@ -1,248 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
 
-int w, h;
-vector<vector<bool>> a;
+vector<int> a;
+int w;
 
-bool check(int x, int y) {
-	for (int i = h - 1; i > x; i--) {
-		if (a[i][y] == false) return false;
+bool isCorrect(int index, string s) {
+	if (index + s.length() > w) return false;
+
+	int standard = a[index] - (s[0] -'0');
+
+	for (int i = 1; i < s.length(); i++) {
+		if (standard != a[index + i] - (s[i] - '0')) return false;
 	}
 	return true;
 }
 
-int play1() {
-	int result = w;
-
-	for (int i = h - 1; i >= 0; i--) {
-		for (int j = 0; j < w - 3; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i][j + 2] || a[i][j + 3]) continue;
-			if (!check(i, j) || !check(i, j + 1) || !check(i, j + 2) || !check(i, j + 3)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play2() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i + 1][j] || a[i + 1][j + 1]) continue;
-			if (!check(i + 1, j) || !check(i + 1, j + 1)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play3() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j + 1] || a[i][j + 2] || a[i + 1][j] || a[i + 1][j + 1]) continue;
-			if (!check(i + 1, j) || !check(i + 1, j + 1) || !check(i, j + 2)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i + 1][j] || a[i + 1][j + 1] || a[i + 2][j + 1]) continue;
-			if (!check(i + 1, j) || !check(i + 2, j + 1)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play4() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i + 1][j + 1] || a[i + 1][j + 2]) continue;
-			if (!check(i, j) || !check(i + 1, j + 1) || !check(i + 1, j + 2)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i + 1][j] || a[i + 1][j] || a[i + 1][j + 1] || a[i + 2][j]) continue;
-			if (!check(i + 1, j + 1) || !check(i + 2, j)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play5() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i][j + 2] || a[i + 1][j + 1]) continue;
-			if (!check(i, j) || !check(i + 1, j + 1) || !check(i, j + 2)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i + 1][j] || a[i][j + 1] || a[i + 1][j + 1] || a[i + 1][j + 2]) continue;
-			if (!check(i + 1, j) || !check(i + 1, j + 1) || !check(i + 1, j + 2)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i + 1][j] || a[i + 2][j] || a[i + 1][j + 1]) continue;
-			if (!check(i + 2, j) || !check(i + 1, j + 1)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j + 1] || a[i + 1][j + 1] || a[i + 2][j + 1] || a[i + 1][j]) continue;
-			if (!check(i + 1, j) || !check(i + 2, j + 1)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play6() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i + 1][j] || a[i + 1][j + 1] || a[i + 1][j + 2] || a[i][j + 2]) continue;
-			if (!check(i + 1, j) || !check(i + 1, j + 1) || !check(i + 1, j + 2)) continue;
-			result++;
-		}
-	}
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i][j + 2] || a[i + 1][j]) continue;
-			if (!check(i + 1, j) || !check(i, j + 1) || !check(i, j + 2)) continue;
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i + 1][j] || a[i + 2][j] || a[i + 2][j + 1]) continue;
-			if (!check(i + 2, j) || !check(i + 2, j + 1)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i + 1][j + 1] || a[i + 2][j + 1]) continue;
-			if (!check(i, j) || !check(i + 2, j + 1)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int play7() {
-	int result = 0;
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j] || a[i + 1][j] || a[i + 1][j + 1] || a[i + 1][j + 2]) continue;
-			if (!check(i + 1, j) || !check(i + 1, j + 1) || !check(i + 1, j + 2)) continue;
-			result++;
-		}
-	}
-
-	for (int i = h - 2; i >= 0; i--) {
-		for (int j = 0; j < w - 2; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i][j + 2] || a[i + 1][j + 2]) continue;
-			if (!check(i, j) || !check(i, j + 1) || !check(i + 1, j + 2)) continue;
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j] || a[i][j + 1] || a[i + 1][j] || a[i + 2][j]) continue;
-			if (!check(i + 2, j) || !check(i, j + 1)) continue;
-
-			result++;
-		}
-	}
-
-	for (int i = h - 3; i >= 0; i--) {
-		for (int j = 0; j < w - 1; j++) {
-			if (a[i][j + 1] || a[i + 1][j + 1] || a[i + 2][j + 1] || a[i + 2][j]) continue;
-			if (!check(i + 2, j) || !check(i + 2, j + 1)) continue;
-
-			result++;
-		}
-	}
-	return result;
-}
-
-int playGame(int index) {
-	switch (index) {
-	case 1:
-		return play1();
-	case 2:
-		return play2();
-	case 3:
-		return play3();
-	case 4:
-		return play4();
-	case 5:
-		return play5();
-	case 6:
-		return play6();
-	case 7:
-		return play7();
-	}
-}
-
 int main() {
-	int index, maxHeight = 0;
+	int index, result = 0;
 	cin >> w >> index;
 
-	vector<int> height(w);
+	for (int i = 0; i < w; i++) {
+		int height;
+		cin >> height;
+		a.push_back(height);
+	}
 
 	for (int i = 0; i < w; i++) {
-		cin >> height[i];
-		if (height[i] > maxHeight) maxHeight = height[i];
-	}
-	h = maxHeight + 4;
-	
-	a.assign(h, vector<bool>(w, false));
-	for (int j = 0; j < w; j++) {
-		for (int i = 1; i <= height[j]; i++) {
-			a[h - i][j] = true;
+		if (index == 1) {
+			if (isCorrect(i, "0")) result++;
+			if (isCorrect(i, "0000")) result++;
+		}
+		else if (index == 2) {
+			if (isCorrect(i, "00")) result++;
+		}
+		else if (index == 3) {
+			if (isCorrect(i, "001")) result++;
+			if (isCorrect(i, "10")) result++;
+		}
+		else if (index == 4) {
+			if (isCorrect(i, "100")) result++;
+			if (isCorrect(i, "01")) result++;
+		}
+		else if (index == 5) {
+			if (isCorrect(i, "000")) result++;
+			if (isCorrect(i, "101")) result++;
+			if (isCorrect(i, "01")) result++;
+			if (isCorrect(i, "10")) result++;
+		}
+		else if (index == 6) {
+			if (isCorrect(i, "000")) result++;
+			if (isCorrect(i, "011")) result++;
+			if (isCorrect(i, "00")) result++;
+			if (isCorrect(i, "20")) result++;
+		}
+		else {
+			if (isCorrect(i, "000")) result++;
+			if (isCorrect(i, "110")) result++;
+			if (isCorrect(i, "00")) result++;
+			if (isCorrect(i, "02")) result++;
 		}
 	}
 
-	cout << playGame(index) << "\n";
+	cout << result << "\n";
 	return 0;
 }
