85f8a1f9bc7c1cb48d24e7533ed38589c80a75a4
Jongwon Choi <cjw--@nate.com>
2019-11-08 16:57:04 +0900

    [add] 2020 카카오 공채 - 가사검색
    나름 효율성을 고려해서 작성했는데 정확성과 효율성 몇 개의 테스트 케이스는 통과하나, 전체 정답을 받진 못한다. Trie 자료구조를 사용하면 문제를 해결할 수 있다는데,  추후 변경이 필요하다.

---
+++ Algorithm/카카오 기출 문제/2020공채_가사검색.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> words, vector<string> queries) {
+	vector<int> answer;
+	map<string, int> result;
+	int query_size = queries.size();
+	int word_size = words.size();
+
+	for (int i = 0; i < query_size; i++) {
+		string query = queries[i];
+
+		if (result.count(query) == 0) {
+			int length = query.length();
+			int start = query.find_first_of('?');
+			int end = query.find_last_of('?');
+			int count = 0;
+
+			if (start > length  && end > length) {
+				for (int j = 0; j < word_size; j++) {
+					if (words[j].length() != length) continue;
+					if (query.compare(words[j]) == 0) {
+						count++;
+					}
+				}
+				answer.push_back(count);
+			}
+			else {
+				if (start > 0) {
+					for (int j = 0; j < word_size; j++) {
+						if (length != words[j].length()) continue;
+						bool check = true;
+
+						for (int k = 0; k < start; k++) {
+							if (query[k] != words[j][k]) {
+								check = false;
+								break;
+							}
+						}
+
+						if (check) {
+							count++;
+						}
+					}
+				}
+				else {
+					for (int j = 0; j < word_size; j++) {
+						if (length != words[j].length()) continue;
+						bool check = true;
+
+						for (int k = end + 1; k < length; k++) {
+							if (query[k] != words[j][k]) {
+								check = false;
+								break;
+							}
+						}
+
+						if (check) {
+							count++;
+						}
+					}
+				}
+				answer.push_back(count);
+			}
+		}
+		else {
+			answer.push_back(result.find(query)->second);
+		}
+	}
+	return answer;
+}
+
+int main() {
+	vector<string> words = { "frodo", "front", "frost", "frozen", "frame", "kakao" };
+	vector<string> queries = { "fro??", "????o", "fr???", "fro???", "pro?" };
+	
+	vector<int> result = solution(words, queries);
+	
+	cout << "\n";
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
